83f0a23f8e5c822f6da40916f92c527e90815974
therandomspoon <107148755+therandomspoon@users.noreply.github.com>
2023-05-18 17:52:40 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,25 @@
+# Squirdle
+
+A Pokémon Wordle-like, found at [squirdle.fireblend.com](http://squirdle.fireblend.com/)
+
+Now running entirely on Javascript!
+
+Featured on [Kotaku](https://kotaku.com/wordle-squirdle-daily-puzzle-word-game-free-browser-1848499480), [GamesRadar](https://www.gamesradar.com/theres-a-pokemon-themed-wordle-called-drumroll-squirdle/), [Forbes](https://www.forbes.com/sites/paultassi/2022/02/09/another-wordle-alternative-squirdle-wants-you-to-guess-that-pokemon/?sh=6a7996747b94), [TechRadar](https://www.techradar.com/sg/news/games-like-wordle-the-best-spinoffs-and-alternatives) and others!
+
+<img src="https://i.imgur.com/nbHjRow.png" width=465px>
+
+Pokémon data based on [Mario Tormo Romero's Pokemon dataset](https://www.kaggle.com/mariotormo/complete-pokemon-dataset-updated-090420).
+
+Pull requests welcome!
+### Running Locally
+You should be able to run this on any local server. Here are [some options](https://medium.com/swlh/need-a-local-static-server-here-are-several-options-bbbe77e59a11)!
+
+### Deploying to AWS
+#### Requirements
+* Make sure you can run the app locally first.
+* An AWS account.
+
+#### Steps:
+Follow [this guide](https://docs.aws.amazon.com/AmazonS3/latest/userguide/HostingWebsiteOnS3Setup.html)!
+
+If you need any help, feel free to lmk!
---
+++ scripts/renderer.js
@@ -0,0 +1,131 @@
+import {getCookie, setCookie} from "./utils.js";
+import {getRevPkmnName} from "./i18n.js";
+import {getPokemonFromId} from "./pokedex.js";
+
+function getTitle(streak) {
+    if (streak < 3){
+        return "Novice Trainer"
+    }
+    if (streak < 7){
+        return "Pokémon Trainer"
+    }
+    if (streak < 10){
+        return "Ace Trainer"
+    }
+    if (streak < 15){
+        return "Pokémon Collector"
+    }
+    if (streak < 25){
+        return "Pokemaniac"
+    }
+    if (streak < 35){
+        return "Pokémon Professor"
+    }
+    if (streak < 45){
+        return "Gym Leader"
+    }
+    if (streak < 60){
+        return "Elite Four"
+    }
+    if (streak < 75){
+        return "Pokémon Champion"
+    }
+    return "Pokémon Master"
+}
+
+export function showState(daily) {
+    let enabled = getCookie("hintsenabled", false)
+    document.getElementById("toggleinfo").innerHTML = "📋 Pokémon Info " + (enabled == "0" ? "OFF" : "ON");
+
+    let guesses = getCookie("guessesv2", daily)
+    let attempts = getCookie("t_attempts", daily)
+
+    guesses = guesses == "" ? [] : JSON.parse(guesses)
+    let guessesCont = document.getElementById("guesses")
+    let hintTitles = document.getElementById("hinttitles")
+
+    if (guesses.length > 0) {
+        if (guessesCont.style.display == "none") {
+            guessesCont.style.display = "block";
+            window.getComputedStyle(hintTitles).opacity;
+            hintTitles.className += ' in';
+        }
+    } else {
+        guessesCont.style.display = "none"
+        hintTitles.className = 'row';
+    }
+    let lastAttempt = ""
+
+    for (const [index, guess] of guesses.entries()) {
+        if (!(document.getElementById('guess' + index) || false)) {
+            lastAttempt = getPokemonFromId(guess.name)
+
+            var rowElement = createElement({ Tag: "div", id: 'guess' + index, classList: 'row' })
+
+            for (const hint of guess.hints) {
+                var img = createElement({ Tag: "img", classList: 'emoji', src: hint })
+                var colElement = createElement({ Tag: "div", classList: 'column', childNodes: [img] })
+                rowElement.appendChild(colElement)
+            }
+            var pokename = createElement({ Tag: "p", classList: 'guess', innerHTML: lastAttempt })
+            var pokeinfo = createElement({ Tag: "span", classList: 'tooltiptext', innerHTML: guess.info })
+            var tooltip = createElement({ Tag: "div", classList: 'tooltip', childNodes: [pokename, pokeinfo] })
+            var colElement = createElement({ Tag: "div", classList: 'column', childNodes: [tooltip] })
+
+            rowElement.appendChild(colElement)
+
+            guessesCont.appendChild(rowElement);
+            window.getComputedStyle(rowElement).opacity;
+            rowElement.className += ' in';
+
+            let guessedPoke = pokedex[getRevPkmnName(lastAttempt)]
+            let type1correct = guess.mosaic[1] == "1" | guess.mosaic[1] == "4"
+            let type2correct = guess.mosaic[2] == "1" | guess.mosaic[2] == "4"
+
+            let type1elem = document.getElementById("type_" + guessedPoke[1])
+            let type2elem = document.getElementById("type_" + guessedPoke[2])
+            type1elem.style.opacity = type1correct ? "1" : "0.12";
+            type1elem.style.borderStyle = type1correct ? "solid" : "none";
+            type2elem.style.opacity = type2correct ? "1" : "0.12";
+            type2elem.style.borderStyle = type2correct ? "solid" : "none";
+        }
+    }
+
+    let secret_name = getPokemonFromId(getCookie("secret_poke", daily).replace(/"/g, ''));
+    if (secret_name == lastAttempt) {
+        document.getElementById("secretpoke").innerHTML = secret_name
+        document.getElementById("guessform").style.display = "none";
+        document.getElementById("results").style.display = "block";
+        document.getElementById("won").style.display = "block";
+        if (daily) {
+            let streak = parseInt(getCookie("streak", false))
+            let title = getTitle(streak)
+            document.getElementById("streak").innerHTML = "You've guessed <b>"+streak+" Pokémon</b> in a row!<br><b>Title: </b>"+title
+        }
+    }
+    else if (guesses.length == attempts) {
+        document.getElementById("secretpoke").innerHTML = secret_name
+        document.getElementById("guessform").style.display = "none";
+        document.getElementById("results").style.display = "block";
+        document.getElementById("lost").style.display = "block";
+        if (daily) {
+            setCookie("streak", 0, 300, false)
+            document.getElementById("streak").innerHTML = "Streak Reset!<br><b>Title:</b> Novice Trainer"
+        }
+    }
+    document.getElementById("attempts").innerHTML = attempts - guesses.length
+}
+
+function createElement(initObj) {
+    let element = document.createElement(initObj.Tag);
+    for (let prop in initObj) {
+        if (prop === "childNodes") {
+            initObj.childNodes.forEach(function (node) { element.appendChild(node); });
+        }
+        else if (prop === "attributes") {
+            initObj.attributes.forEach(function (attr) { element.setAttribute(attr.key, attr.value) });
+        }
+        else element[prop] = initObj[prop];
+    }
+    return element;
+}
